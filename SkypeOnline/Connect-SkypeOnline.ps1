e4c2a6cf24e3f1d618979dd6e731e20538ed280a
joaoalhinho <joao.alhinho@hotmail.com>
2017-12-28 21:56:26 +0100

    Added Connect-SkypeOnline Script

---
+++ SkypeOnline/Connect-SkypeOnline.ps1
@@ -0,0 +1,21 @@
+﻿function Connect-SkypeOnline {
+    param(
+     [parameter(Mandatory=$true)]
+     [ValidateSet("TAP", "Lab", "CCE")]
+     [String[]]$Tenant
+    )    
+
+If ($Tenant -eq "TAP"){
+    Import-PSSession (New-CsOnlineSession -UserName joao.alhinho@tap.swisscom.com -OverrideAdminDomain tap4swisscom.onmicrosoft.com)
+}
+ElseIf ($Tenant -eq "Lab"){
+    $password=get-content -Path "LabCred.txt"  | convertto-securestring
+    $credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "admin@alhinho.onmicrosoft.com",$password
+    
+    Import-Pssession (New-CsOnlineSession -Credential (Get-Credential $credentials)) -AllowClobber
+}elseif($Tenant -eq "CCE"){
+    Write-Host "CCE not yet deployed"
+    Return
+}
+    
+}
