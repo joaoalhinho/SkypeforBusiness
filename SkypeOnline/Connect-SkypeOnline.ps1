a61294c5828a6d91cfedd752143d97c15cdfe284
joaoalhinho <joao.alhinho@hotmail.com>
2018-05-23 15:24:24 +0200

    allow clobber added

--- SkypeOnline/Connect-SkypeOnline.ps1
+++ SkypeOnline/Connect-SkypeOnline.ps1
@@ -1,12 +1,14 @@
 ﻿function Connect-SkypeOnline {
     param(
      [parameter(Mandatory=$true)]
-     [ValidateSet("TAP", "Lab", "CCE")]
+     [ValidateSet("TAP", "Lab", "CCE","LabOnprem")]
      [String[]]$Tenant
-    )    
+    )  
+    
+Import-Module SkypeOnlineConnector
 
 If ($Tenant -eq "TAP"){
-    Import-PSSession (New-CsOnlineSession -UserName joao.alhinho@tap.swisscom.com -OverrideAdminDomain tap4swisscom.onmicrosoft.com)
+    Import-PSSession (New-CsOnlineSession -UserName joao.alhinho@tap.swisscom.com -OverrideAdminDomain tap4swisscom.onmicrosoft.com) -AllowClobber
 }
 ElseIf ($Tenant -eq "Lab"){
     $password=get-content -Path "LabCred.txt"  | convertto-securestring
@@ -16,6 +18,12 @@ ElseIf ($Tenant -eq "Lab"){
 }elseif($Tenant -eq "CCE"){
     Write-Host "CCE not yet deployed"
     Return
+}elseif($Tenant -eq "LabOnprem"){
+    $credentials=Get-Credential -UserName "lab\adm-ja" -Message "Please enter password"
+    $session= New-PSSession -ConnectionUri "https://ucadmin.alhinho.com/OcsPOwershell" -Credential $credentials
+    Import-PSSession -Session $session
+
 }
     
 }
+Connect-SkypeOnline
